c9ba0915a368130a85dcfc5c27556d92c6f45dc5
emotion3459 <176516814+emotion3459@users.noreply.github.com>
2026-03-23 17:15:39 -0400

    Add files

---
+++ updater.bat
@@ -0,0 +1,22 @@
+@echo off
+set scripts_dir=.\scripts\
+set shaders_dir=.\shaders\
+
+set scripts=(^
+	"https://raw.githubusercontent.com/mpv-player/mpv/master/TOOLS/lua/autocrop.lua"^
+	"https://raw.githubusercontent.com/po5/trackselect/master/trackselect.lua"^
+	"https://raw.githubusercontent.com/Dudemanguy/mpv-manga-reader/master/manga-reader.lua"^
+)
+
+set shaders=(^
+	"https://raw.githubusercontent.com/Artoriuz/ArtCNN/main/GLSL/ArtCNN_C4F32.glsl"^
+)
+
+if not exist %scripts_dir% mkdir %scripts_dir%
+if not exist %shaders_dir% mkdir %shaders_dir%
+
+for %%a in %scripts% do powershell -Command "Invoke-WebRequest -Uri %%a -OutFile "%scripts_dir%%%~nxa""
+for %%a in %shaders% do powershell -Command "Invoke-WebRequest -Uri %%a -OutFile "%shaders_dir%%%~nxa""
+
+echo Updating Finished
+timeout 3
